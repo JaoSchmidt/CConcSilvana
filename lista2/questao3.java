38b04ee04df6d951b9f8a75837d73cc2b02fe252
JaoSchmidt <jhsc98@gmail.com>
2021-09-27 15:46:12 -0300

    lista 2 feita

--- lista2/questao3.java
+++ lista2/questao3.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 class FilaTarefas {
 
@@ -28,12 +29,19 @@ class FilaTarefas {
 		synchronized(queue){ 
 			this.shutdown=true;
 		}
+		new WaitAndForce().run();
 		for (int i=0; i<nThreads; i++)
 			try { 
 				threads[i].join(); 
 			} catch (InterruptedException e) { 
 				return;
 		 	}
+	}
+	class WaitAndForce implements Runnable{
+		public void run(){
+			try { Thread.sleep(5000); } catch (InterruptedException e){}
+			Tarefa.shut(); 
+		}
 	}
 	private class MyPoolThreads extends Thread {
 		public void run() {
@@ -47,28 +55,50 @@ class FilaTarefas {
 					}
 					//retira o primeiro elemento da lista e o retorna
 					if(queue.isEmpty() && shutdown) return;
-						r = (Runnable) queue.removeFirst();
-				}	
+					r = (Runnable) queue.removeFirst();
+				}
 				try { r.run(); } catch (RuntimeException e) {}
-			}
-		} 
+			} 
+		}
 	}
-	static class Tarefa implements Runnable{
+	static class Tarefa{
+		static boolean mustRun=true;
+		public static void shut(){
+			mustRun=false;
+		};
+	}
+	static class TarefaFinita extends Tarefa implements Runnable{
 		int id;
-		Tarefa(int id){
+		TarefaFinita(int id){
 			this.id=id;
 		}
 		public void run(){
 			int bob1=1000,bob2=1000000;
 			for(;bob1<bob2;bob1++){}
-			System.out.println(id);
+			System.out.println("Tarefa "+id+" parando");
+		}
+	}
+	static class TarefaInfinita extends Tarefa implements Runnable{
+		int id;
+		TarefaInfinita(int id){
+			this.id=id;
+		}
+		public void run(){
+			while(mustRun){
+				try{
+				System.out.println("Tarefa "+id+" continua rodando");
+				Thread.sleep(3000);//dorme por 3 segundos
+				}catch (InterruptedException e){}
+			}
+			System.out.println("Tarefa "+id+" parando");
 		}
 	}
 	public static void main(String[] args){
 		FilaTarefas a = new  FilaTarefas(10);
 		//System.out.println("Id das tarefas executadas:");
-		for(int i=0;i<5;i++)
-			a.execute(new Tarefa(i));	
+		for(int i=0;i<99;i++)
+			a.execute(new TarefaFinita(i));	
+		a.execute(new TarefaInfinita(99));
 		a.shutdown();
 	}
 }
